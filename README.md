c1ef67da55bf633b206b10edcb0ba220f3823c63
Charlie O'Leary <charlie@lever.co>
2019-01-15 16:17:17 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,44 @@
+# globaldog
+
+Ever want to convert an existing Datadog screenboard to use the new global time option? This script
+will make it a much easier task.
+
+### Installing
+
+Simply clone the repository, run `pip install -r requirements.txt`, then issue the command below.
+
+## Getting Started
+
+First, visit the [Datadog API page](https://app.datadoghq.com/account/settings#api) and generate or
+grab an existing API and application key. Once you have that, you can set the following environment
+variables:
+
+```shell
+export DATADOG_API_KEY=<YOUR_API_KEY>
+export DATADOG_APP_KEY=<YOUR_APP_KEY>
+```
+
+Next, open a Datadog screenboard that you wish to use global time with. Take note of the ID in the
+URL as shown below. If this is an integration dashboard included with Datadog, you'll need to clone
+it, then copy the new ID.
+
+```shell
+https://app.datadoghq.com/screen/<SCREENBOARD_ID>/title-of-your-dashboard?page=0&is_auto=false&from_ts=1547592360000&to_ts=1547595960000&live=true
+```
+
+Once you have your ID simply run:
+
+```shell
+python globaldog.py --screenboard <SCREENBOARD_ID>
+```
+
+The script will fetch the board, update all widgets with the `time` key, then push the modified
+dashboard back up to your Datadog account.
+
+## Authors
+
+* **Charlie O'Leary** - [charlieoleary](https://github.com/charlieoleary)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ globaldog.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+import json
+import argparse
+import os
+
+try:
+    from datadog import initialize, api
+except ImportError:
+    print(
+        "The `datadog` module is not installed. Install it with `pip install datadog`."
+    )
+    exit(1)
+
+
+def handleErrors(datadogResponse):
+    """ returns a list of datadog errors for the user to correct
+    """
+    if datadogResponse.get("errors"):
+        print("One or more errors occurred:")
+        for error in datadogResponse.get("errors"):
+            print("- %s" % error)
+        exit(1)
+
+
+datadogCredentials = {
+    "api_key": os.environ.get("DATADOG_API_KEY"),
+    "app_key": os.environ.get("DATADOG_APP_KEY"),
+}
+
+initialize(**datadogCredentials)
+
+parser = argparse.ArgumentParser(
+    description="A simple script that converts all widgets to use global time."
+)
+
+parser.add_argument(
+    "-s",
+    "--screenboard",
+    required=True,
+    help="The screenboard ID that you wish to convert.",
+)
+
+args = parser.parse_args()
+
+print("Starting conversion of screenboard %s..." % args.screenboard)
+
+screenboard = api.Screenboard.get(args.screenboard)
+
+handleErrors(screenboard)
+
+# iterate over all widgets in the dashboard and set the `time` key to an empty dictionary.
+for widget in screenboard["widgets"]:
+    if widget.get("time"):
+        widget["time"] = {}
+        print("+ Updated '{}' widget..." % widget["title_text"])
+
+# append the global time remark to the dashboard title.
+screenboard["board_title"] = "{} {}".format(screenboard["board_title"], "(Global Time)")
+
+print("Sending updated screenboard to Datadog...")
+
+update_screenboard = api.Screenboard.update(
+    args.screenboard,
+    board_title=screenboard["board_title"],
+    widgets=screenboard["widgets"],
+    template_variables=screenboard["template_variables"],
+)
+
+handleErrors(update_screenboard)
+
+print("Your screenboard has been updated! You can view it here:")
+print("https://app.datadoghq.com/screen/%s" % args.screenboard)
+exit()
